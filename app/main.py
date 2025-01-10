178e92d7dc26c9b4fa946485344b1fa107d1c237
Rui Yi Gan <kurtganruiyi@gmail.com>
2025-01-10 16:44:40 +0800

    Respond with body

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
 
     client_sock, client_addr = (
         server_socket.accept()
-    )  # wait for client returns client socket and address once client is connected
+    )  # Wait for client returns client socket and address once client is connected
 
     with client_sock:
         while True:
@@ -20,13 +20,25 @@ def main():
             )  # HOW TO: https://realpython.com/python-sockets/
             if not data:
                 break
-            data = data.decode("utf-8")
+            data = data.decode(
+                "utf-8"
+            )  # Specify endcoding to tell python how to read the bytes
             data_split = data.split()
             path = data_split[1]
             if path == "/":
                 client_sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
             else:
-                client_sock.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                if path[:6] == "/echo/":
+                    echo_string = path[6:]
+                    client_sock.sendall(
+                        f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_string)}\r\n\r\n{echo_string}".encode(
+                            "utf-8"
+                        )
+                    )
+                else:
+                    client_sock.sendall(
+                        b"HTTP/1.1 404 Not Found\r\n\r\n"
+                    )  # Don’t see the \r\n\r\n when decoding b"HTTP/1.1 404 Not Found\r\n\r\n" is that \r\n (carriage return + line feed) is a special sequence used for formatting
     server_socket.close()
 
 
